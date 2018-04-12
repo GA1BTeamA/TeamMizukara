0ab4e260e178d4d75e24c5d9d72870be9cf357d2
student <student@DEV1-4>
2018-04-12 10:59:24 +0900

    Hero最新

---
+++ Mizukara/Mizukara/GameEngine/Hero.cpp
@@ -0,0 +1,67 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+#include "..\Hero.h"
+
+CHero::CHero()
+	:m_x(200), m_y(400),
+	m_vx(0.0f),m_vy(0.0f),
+	m_direc(RIGHT)
+{
+	//HEROオブジェクトの各当たり判定の属性をバラバラにする
+	static int count = 0;
+	count++;
+
+	//ヒットボックス作成()
+	m_p_hit_box=Collision::HitBoxInsert(this);
+	//作成したヒットボックスの値を設定
+	m_p_hit_box->SetPos(m_x, m_y);
+	m_p_hit_box->SetWH(60.0f, 100.0f);
+	m_p_hit_box->SetElement(count);		//属性をcountにする
+	m_p_hit_box->SetInvisible(false);	//無敵モード無効
+}
+
+CHero::~CHero()
+{
+
+}
+
+void CHero::Action()
+{
+	//移動
+	if (Input::KeyPush(VK_LEFT))
+	{
+		m_x -= 5.0f;
+		m_direc = LEFT;
+	}
+	else if (Input::KeyPush(VK_RIGHT))
+	{
+		m_x += 5.0f;
+		m_direc = RIGHT;
+	}
+
+
+	//当たり判定の位置更新
+	m_p_hit_box->SetPos(m_x, m_y);
+}
+
+void CHero::Draw()
+{
+	//描画
+	if (Input::KeyPush(VK_LEFT))		//左
+	{
+		Draw::Draw2D(3, m_x, m_y);
+	}
+	else if (Input::KeyPush(VK_RIGHT))	//右
+	{
+		Draw::Draw2D(2, m_x, m_y);
+	}
+	else {								//停止中
+		if(m_direc==LEFT)
+			Draw::Draw2D(1, m_x, m_y);
+		else
+			Draw::Draw2D(0, m_x, m_y);
+	}
+
+}//
