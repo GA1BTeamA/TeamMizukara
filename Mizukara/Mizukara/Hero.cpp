7ab0464869838f6b67b3d98ac2f3b38f6cdedc33
student <student@DEV1-11>
2018-06-06 17:11:01 +0900

    卍

--- Mizukara/Mizukara/Hero.cpp
+++ Mizukara/Mizukara/Hero.cpp
@@ -869,6 +869,7 @@ void CHero::Action()
 						{
 							if (m_CKey_Frag == false)
 								Audio::StartLoopMusic(1);
+							Audio::LoopMusicVolume(1, 0.03f);
 
 							m_CKey_Frag = true;
 						}
